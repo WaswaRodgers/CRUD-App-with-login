0451ab688807f5bccb1ce270ee447ba850e997a4
waswaRodgers <waswam1994@gmail.com>
2020-07-05 23:31:43 +0300

    Created empty files

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer>
+	<address>
+		Designed by <a href="#">Waswa Rodgers</a><br/>
+		Current Location: Kangemi, Westlands.<br/>
+		Home Address: Ndivisi, Webuye East.<br/>
+		Phone: 0702261867.<br/>
+	</address>
+</footer>
+</html>
