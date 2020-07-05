8b29c3b0b9ebcc226c6dab3d5d9881a9aed460c9
waswaRodgers <waswam1994@gmail.com>
2020-07-05 23:43:57 +0300

    Added nav bar to index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<div class="container">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <a class="navbar-brand" href="#">Waswa Rodgers</a>
+
+		  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+		    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">About</a>
+		      </li>
+		      <div class="dropdown"> 
+		      	<button type="button" class="btn btn-dark dropdown-toggle" data-toggle="dropdown">Dropdown</button>
+		      	<!--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span>
+		      	</a> -->
+		      	<div class="dropdown-menu"> 
+		      		<a class="dropdown-item" href="#">Action</a> 
+		      		<a class="dropdown-item" href="#">Another action</a> 
+		      		<a class="dropdown-item" href="#">Something else here</a> 
+		      		<li role="separator" class="dropdown-divider"></li> 
+		      		<a class="dropdown-item" href="#">Separated link</a> 
+		      		<li role="separator" class="dropdown-divider"></li> 
+		      		<a class="dropdown-item" href="#">One more separated link</a> 
+		      	</div> 
+		      </div> 
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Services</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Blog</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Contacts</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+		      </li>
+		    </ul>
+		    <form class="form-inline my-2 my-lg-0">
+		      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+		      <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+		    </form>
+		  </div>
+		</nav> 
+</div>
