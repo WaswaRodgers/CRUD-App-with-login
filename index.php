a85aa102b0f0d02561febfced38cfa29bd8e30a7
waswaRodgers <waswam1994@gmail.com>
2020-07-06 07:35:25 +0300

    Added brand name 'CRUD App with Login'

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
 		    <span class="navbar-toggler-icon"></span>
 		  </button>
-		  <a class="navbar-brand" href="#">Waswa Rodgers</a>
+		  <a class="navbar-brand" href="#">CRUD App with Login</a>
 
 		  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
 		    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
@@ -49,6 +49,6 @@
 		  </div>
 		</nav> 
 
-		
+
 <?php include_once('includes/footer.php') ?>
 </div>
